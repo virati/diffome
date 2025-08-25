0466916e1ceb33c622ae3a75078db19c0652627e
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-08-24 18:30:12 -0700

    cleanup dipy pull

--- scripts/subatlas_tda.py
+++ scripts/subatlas_tda.py
@@ -14,15 +14,16 @@ from dipy.data.fetcher import (fetch_file_formats,
 input_trk: Path = "/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_left.trk"
 ref_anat_filename = None
 
-fetch_file_formats()
-bundles_filename, ref_anat_filename = get_file_formats()
-reference_anatomy = nib.load(ref_anat_filename)
 
-main_connectome = Connectome(bundles_filename[0], ref = reference_anatomy)
+def pull_dipy_trk():
+    fetch_file_formats()
+    bundles_filename, ref_anat_filename = get_file_formats()
+    reference_anatomy = nib.load(ref_anat_filename)
+
+    return bundles_filename[0], reference_anatomy
+
+main_connectome = Connectome(*pull_dipy_trk())
 for ii in range(10):
     main_connectome.subsample(factor=1000)
     barcode_analysis = BarCode(main_connectome)
     barcode_analysis.calculate(do_plot = True)
-
-
-#%%
