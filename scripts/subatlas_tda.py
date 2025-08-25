b7650d6b07f4924788f86a7c47e97adf694c2348
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-08-25 07:43:40 -0700

    more prototyping

--- scripts/subatlas_tda.py
+++ scripts/subatlas_tda.py
@@ -11,10 +11,6 @@ import os
 from dipy.data.fetcher import (fetch_file_formats,
                                get_file_formats)
 
-input_trk: Path = "/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_left.trk"
-ref_anat_filename = None
-
-
 def pull_dipy_trk():
     fetch_file_formats()
     bundles_filename, ref_anat_filename = get_file_formats()
@@ -22,8 +18,30 @@ def pull_dipy_trk():
 
     return bundles_filename[0], reference_anatomy
 
-main_connectome = Connectome(*pull_dipy_trk())
-for ii in range(10):
-    main_connectome.subsample(factor=1000)
-    barcode_analysis = BarCode(main_connectome)
-    barcode_analysis.calculate(do_plot = True)
+#%%
+input_trk_left: Path = "/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_left.trk"
+input_trk_right: Path = "/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_right.trk"
+ref_anat_filename = 'same'
+
+petersen_subatlas_left = (input_trk_left, ref_anat_filename)
+petersen_subatlas_right = (input_trk_right, ref_anat_filename)
+tract_list = {'left': petersen_subatlas_left, 'right': petersen_subatlas_right,}
+
+barcode_stack = {key: [] for key in tract_list}
+for key, val in tract_list.items():
+    main_connectome = Connectome(*val)
+    for ii in range(10):
+        main_connectome.subsample(factor=500)
+        barcode_analysis = BarCode(main_connectome)
+        barcode_analysis.calculate(do_plot = False)
+        barcode_stack[key].append(barcode_analysis.barcode)
+
+#%%
+aggregate_barcode = {}
+import matplotlib.pyplot as plt
+for side in ['left','right']:
+    aggregate_barcode[side] = [[barcode_stack[side][iter][element][1] for element in range(len(barcode_stack[side][iter]))] for iter in range(10)]
+    aggregate_barcode[side] = [item for sublist in aggregate_barcode[side] for item in sublist]
+    xval = [item[0] for item in aggregate_barcode[side]]
+    yval = [item[1] for item in aggregate_barcode[side]]
+    plt.scatter(xval, yval,alpha=0.5)
