aa39ee0203d932e70ff07dc787285c2adcde4d1a
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-08-26 15:40:05 -0700

    dict-ify

--- scripts/subatlas_tda.py
+++ scripts/subatlas_tda.py
@@ -19,30 +19,41 @@ def pull_dipy_trk():
     return bundles_filename[0], reference_anatomy
 
 #%%
-#input_trk_left: Path = "/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_left.trk"
-#input_trk_right: Path = "/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_right.trk"
-
-#input_trk_left: Path = "/home/virati/Data/postdoc/subatlases/july_run_hcp_bilat/july_run_hcp_bilat_100_mirrorPD_top_left.trk"
-#input_trk_right: Path = "/home/virati/Data/postdoc/subatlases/july_run_hcp_bilat/july_run_hcp_bilat_100_mirrorPD_top_right.trk"
-
-input_trk_left: Path = "/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100_mirrorPD_top_left.trk"
-input_trk_right: Path = "/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100_mirrorPD_top_right.trk"
-
+input_trk_paths = {
+    "petersen": {
+        "left": ("/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_left.trk", "same"),
+        "right": ("/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_right.trk", "same"),
+    },
+    "hcp": {
+        "left": ("/home/virati/Data/postdoc/subatlases/july_run_hcp_bilat/july_run_hcp_bilat_100_mirrorPD_top_left.trk", "same"),
+        "right": ("/home/virati/Data/postdoc/subatlases/july_run_hcp_bilat/july_run_hcp_bilat_100_mirrorPD_top_right.trk", "same"),
+    },
+    "mgh": {
+        "left": ("/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100_mirrorPD_top_left.trk", "same"),
+        "right": ("/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100_mirrorPD_top_right.trk", "same"),
+    },
+}
 
 #%%
 ref_anat_filename = 'same'
-petersen_subatlas_left = (input_trk_left, ref_anat_filename)
-petersen_subatlas_right = (input_trk_right, ref_anat_filename)
-tract_list = {'left': petersen_subatlas_left, 'right': petersen_subatlas_right,}
+connectome_name = 'petersen'
+#petersen_subatlas_left = (input_trk_left, ref_anat_filename)
+#petersen_subatlas_right = (input_trk_right, ref_anat_filename)
+#tract_list = {'left': petersen_subatlas_left, 'right': petersen_subatlas_right,}
+
+tract_list = input_trk_paths[connectome_name]
 
 barcode_stack = {key: [] for key in tract_list}
+barcode_stack_culprit = {key: [] for key in tract_list}
 for key, val in tract_list.items():
     main_connectome = Connectome(*val)
     for ii in range(100):
         main_connectome.clip_streamlines(n_clip=100)
         barcode_analysis = BarCode(main_connectome)
-        barcode_analysis.calculate(do_plot = False, ignore_streamlines=[ii], downsample_points=50)
+        barcode_analysis.calculate(do_plot = False, ignore_streamlines=[ii], downsample_points=100)
         barcode_stack[key].append(barcode_analysis.barcode)
+        barcode_stack_culprit[key].append(ii)
+        #add in the index itself here, so we can start doing streamline attribution?
 
 #%%
 aggregate_barcode = {}
@@ -85,3 +96,17 @@ first_barcode = np.array(aggregate_barcode['left'])
 second_barcode = np.array(aggregate_barcode['right'])
 test = wass_dist(first_barcode, second_barcode)
 print(test)
+
+#%%
+#Inter Hemi Distances
+inter_dist = []
+for ii in range(len(barcode_stack['left'])):
+    first_barcode = np.array([b for a,b in barcode_stack['left'][ii]])
+    for jj in range(ii+1, len(barcode_stack['right'])):
+        if ii == jj:
+            continue
+        second_barcode = np.array([b for a,b in barcode_stack['right'][jj]])
+        test = wass_dist(first_barcode, second_barcode)
+        inter_dist.append(test)
+
+plt.hist(inter_dist, bins=30, alpha=0.5)
