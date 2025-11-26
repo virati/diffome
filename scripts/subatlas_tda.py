5fc5d7fc9a979fe281bba967f2549fd5be3a6e3e
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-11-26 13:32:03 -0800

    unclear

--- scripts/subatlas_tda.py
+++ scripts/subatlas_tda.py
@@ -23,16 +23,21 @@ input_trk_paths = {
         "left": ("/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100_mirrorPD_top_left.trk", "same"),
         "right": ("/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100_mirrorPD_top_right.trk", "same"),
     },
+    "mgh100":{
+        "left": ("/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100PD_top_right.trk", "same"),
+        "right": ("/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100PD_top_right.trk", "same"),
+    }
 }
 
 #%%#
 ref_anat_filename = 'same'
-connectome_name = 'mgh'
+connectome_name = 'mgh100'
 
 #%%
 tract_list = input_trk_paths[connectome_name]
 #tract_list = ["/home/virati/Data/postdoc/connectome_transfer/petersen_top/petersen_pd_top_right.trk", "/home/virati/Data/postdoc/subatlases/july_run_mgh_bilat/july_run_mgh_bilat_100_mirrorPD_top_right.trk",]
 connectomes = [Connectome(*val).clip_streamlines(n_clip=100) for key, val in tract_list.items()]
+
 TDA_comp = TDAComparison(connectomes)
 
 TDA_comp.calculate()
@@ -49,8 +54,15 @@ if plot_together:
 #%%
 # final calculation
 TDA_comp.calculate_cross_distance()
+
+#%%
+#overlay:
+#connectomes.append(Connectome("/home/virati/Data/postdoc/connectomes/trks/hcp_6k.trk", "same", bbox_valid_check=False).subsample(100))
+
 #%%
 full_render = ConnectomeRenderer(connectomes)
-full_render.render(color_per_bundle = True, connectome_list = [1,2,3])
-
+full_render.render(color_per_bundle = True, do_connectomes = [0,1], show_template=False)
+#full_render.render(color_per_bundle=False, do_connectomes=[2], alpha=0.04)
+from dipy.viz import window
+window.show(full_render.scene, size=(600, 600), reset_camera=False)
 #full_render.render(color_per_bundle=True)
--- src/diffome/viz/__init__.py
+++ src/diffome/viz/__init__.py
@@ -1,6 +1,7 @@
 from dipy.viz import actor, window
 import matplotlib.cm as cm
 from typing import List
+from nilearn.datasets import load_mni152_template
 
 # BUNDLE_COLOR = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0)]
 BUNDLE_COLOR = cm.get_cmap("tab10")
@@ -11,19 +12,36 @@ class ConnectomeRenderer:
         if connectome_list is None:
             connectome_list = []
         self.connectome_list = connectome_list
+        self.scene = window.Scene()
 
-    def render(self, color_per_bundle=False, connectome_list=None):
-        scene = window.Scene()
+    def render(
+        self, color_per_bundle=False, do_connectomes=None, alpha=1.0, show_template=True
+    ):
+        scene = self.scene
+
+        if show_template:
+            mni_t1_template = load_mni152_template()
+            mni_data = mni_t1_template.get_fdata()
+            mni_affine = mni_t1_template.affine
+
+            template_z_slice = actor.slicer(mni_t1_template.dataobj, affine=mni_affine)
+            scene.add(template_z_slice)
 
         for cc, connectome in enumerate(self.connectome_list):
+            if cc not in do_connectomes:
+                continue
             if color_per_bundle:
                 stream_actor = actor.line(
                     connectome.streamlines.streamlines, BUNDLE_COLOR(cc)
                 )
             else:
-                stream_actor = actor.line(connectome.streamlines.streamlines)
+                stream_actor = actor.line(
+                    connectome.streamlines.streamlines, opacity=alpha
+                )
 
             scene.add(stream_actor)
 
         # Uncomment the line below to show to display the window
-        window.show(scene, size=(600, 600), reset_camera=False)
+        # window.show(scene, size=(600, 600), reset_camera=False)
+        self.scene = scene
+        return self
